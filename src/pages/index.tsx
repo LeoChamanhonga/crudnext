c420775d3ee0ddfa7683af91fb188f9c8d4730c9
Leo <nunochev@live.com>
2021-11-14 18:37:57 +0200

    Comp. Layout e titulo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
+import Layout from "../components/Layout"
+
 
 export default function Home() {
   return (
     <div className={`
     flex h-screen justify-center items-center
+    bg-gradient-to-r from-purple-500 to-blue-600
+    text-white
     `}>
-      <span>Texto</span>
+    <Layout titulo ="Cadastro Simples">
+      <span>Content</span>
+    </Layout>
     </div>
   )
 }
